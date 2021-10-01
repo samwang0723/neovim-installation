462c2cf5b8e058dd29c7b1d6dd17eefc142c13e0
Sam Wang <sam.wang.0723@gmail.com>
2021-10-01 12:43:20 +0800

    Add CTags support and Git blamer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,6 +28,46 @@ return require('packer').startup(function()
     use 'itchyny/vim-gitbranch'
     use 'andrewstuart/vim-kubernetes'
     use {
+        'APZelos/blamer.nvim',
+        setup = function()
+            vim.g.blamer_enabled = 1
+        end
+    }
+    use {
+        'preservim/tagbar',
+        cmd = 'TagbarToggle',
+        setup = function()
+            vim.g.tagbar_type_go = {
+                ctagstype = 'go',
+                kinds = {
+                    'p:package',
+                    'i:imports:1',
+                    'c:constants',
+                    'v:variables',
+                    't:types',
+                    'n:interfaces',
+                    'w:fields',
+                    'e:embedded',
+                    'm:methods',
+                    'r:constructor',
+                    'f:functions'
+                },
+                sro = '.',
+                kind2scope = {
+                    t = 'ctype',
+                    n = 'ntype',
+                },
+                scope2kind = {
+                    ctype = 't',
+                    ntype = 'n'
+                },
+                ctagsbin  = 'gotags',
+                ctagsargs = '-sort -silent'
+            }
+            vim.api.nvim_set_keymap("n", "<F8>", "<cmd>TagbarToggle<cr>" ,{silent = true, noremap = true})
+        end
+    }
+    use {
         'alvan/vim-closetag',
         setup = function()
             vim.g.closetag_filenames = '*.html,*.xhtml,*.phtml,*.tpl'
