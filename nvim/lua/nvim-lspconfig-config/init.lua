034c2d097dff8e0310c184b6c152bc3a676e3061
Sam Wang <sam.wang.0723@gmail.com>
2023-02-15 15:55:17 +0800

    feat: swap to lua_ls

--- nvim/lua/nvim-lspconfig-config/init.lua
+++ nvim/lua/nvim-lspconfig-config/init.lua
@@ -200,10 +200,17 @@ lsp.dockerls.setup {
 }
 
 -- lua LSP
-lsp.sumneko_lua.setup {
+lsp.lua_ls.setup {
   settings = {
     Lua = {
-      diagnostics = { globals = { 'vim' } },
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim', 'lua' },
+      },
       workspace = {
         -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
